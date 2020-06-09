e7944df0dbdc5b9eed66b3f7806d374bba978c38
Jim Tisdale <blubagoowenischinchilla@gmail.com>
2020-06-09 15:36:35 -0500

    added cors value for react application

--- middleware/index.js
+++ middleware/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
+var corsOptions = {
+  origin: 'https://www.jim-tisdale.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 module.exports = function(app) {
 	app.use(morgan('dev'));
 	app.use(express.json());
